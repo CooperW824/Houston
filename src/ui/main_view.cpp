7b777f96ba9ece9a9c94b75a8f9cd1dbd3ac2f01
Cooper Werner <cooperwerner824@gmail.com>
2025-11-16 09:03:32 -0500

    update title

--- src/ui/main_view.cpp
+++ src/ui/main_view.cpp
@@ -134,7 +134,7 @@ void start_ui(double refresh_rate_seconds)
 
     auto main_view = Renderer(main_container, [&]
                               { return vbox({
-                                           text("Houston - AI Powered System Monitor and Optimizer, MIT LICENSED 2025") | bold | center,
+                                           text("Houston - Machine Learning Powered System Monitor and Optimizer, MIT LICENSED 2025") | bold | center,
                                            separator(),
                                            function_select->Render(),
                                            separator(),
